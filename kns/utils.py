caa80f7dd346a0519eb992b72e4f826a1637ad64
Hanna Zdulska <zdula616@gmail.com>
2021-05-01 14:13:34 +0200

    add coloring

--- kns/utils.py
+++ kns/utils.py
@@ -62,3 +62,128 @@ def test_candidates_for_position(permutation, min_len, available_numbers, positi
             candidates.append(number)
     return candidates
 
+victory = """\033[32m
+XXXXXXXXXXXXXXXXXXKKKKKKKKKKK00OOkkkOOOOOOOOOOOOOOOOOOOOOOOOOOOOkOkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXKKKKKKKK0kdlc:;;,;:clodxxxkkOOOOOOOOOOOOOOOOOOkkkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXXXKKK0Oxl:;,'''''',,,,,;,,,;;cldkOOOOkkkkkkkkkkkkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXXKKK0kdoc:;,,,,,',,,,,,,,''''''';cdkkkkkkkkkkkkkkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXXKKKOkoc:;,,,,,,;::::;;;,'''...'''';lkkxkkkkkkkkkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXXXKKOkoc::::;::lodxxkxdl:,'''.......':;..ckkkkkkkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXXXXKK0xolclodxk0KKKKK0Oko:,,'.... ........okkkkkkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXXXXKOxkOkk0KKKXXXXXXKK0Oxc,'......  ... ..:kkkkkkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXXXKOkk000KKKXXXXXXXKKK0kdc;'.......   .. .,xkkkkkkkkkkkkkk
+XXXXXXXXXXXXXXXXXXXK0kk0000KKKKXXXXKKKKKOxoc;,....... .  . ..dxkkxkkkkkkkkkx
+XXXXXXXXXXXXXXXXXXKK0kO0KKKKKKXXXXXXKKK0Odl:;'....    ..  ..'oxxxxxxxxxxxxxx
+XXXXXXXXXXXXXXXXXXXKK0O000000KKKKKKKKK00Oko:,'..  .....'  ..,lxxxxxxxxxxxxxx
+XXXXXXXXXXXXXXXXXXKK0xxxkkkkxkkOO00000OOOkxl'... . ...'l  . ,cxxxxxxxxxxxxxx
+XXXXXXXXXXXXXXXXXXkokd::lxxol:clddxxxxddlc:,... . ......  . ':xdddoooooooooo
+XXXXXXXXXXXXXXXXXXKxkOolkOoclolccoloodxxxdxdo;. . ....     .................
+XXXXXXXXXXXXXXXXXXXKOkxO0OOxdxxxxkxk000OOOOkxl. .   .       ................
+XXXXXXXXXXXXXXXXXXXKKOO00OOOkxxxkOO000000Okkxo'             ................
+XXXXXXXXXXXXXXXXXXKKKkO0OOOkxodkO0000000OOkkkxc.            .... ..     ... 
+XXXXXXXXXXXXXXXXXKKKK0ddolcclodddxkkkOOOOOkkkxl;.             .           ..
+XXXXXXXXXXXXXXXXKKKKKK0dolcloxxxdodxxkkOOOkkxdl:;;;'.                      .
+XXXXXXXXXXXXXXKKKKKKKKKOxo,:llloodxxkkkkkkkxdolclllc,...                    
+KKKKKKKKKKKKKKKKKKKKKKKKkoclloxxkkkkkkkkxxdolccloddo:..''.                  
+KKKKKKKKKKKKKKKKKKKKKKK00xccloxxkkkxddddooc:::oddddl,..''''....             
+0000000000000K0KKKKKK000Oolccoddxdoollcc::;;ldddxdl;.'..'''....''...        
+00000000000000000KKK000ko;,;cc;;:;,;,,;;;:lodddddl,''',,'.'....,,,,,,'.     
+00000000000000000000ko:,,''........',,,:coodddddo;'''',,'...'.',,,,,,,,,'.  
+OOO000000000000000kc,,''''.........,;:cllooddddo:'',,''.......'''',,,,,,,,,.
+OOOOO000OOOO000Oxc,''''..'.........:cllooodddddc'',,''...''.....''',,,,,,,,,
+OOOOOOOOOOOOOOkc,'''''........'.....:clooddddoc,'''''............''''',,,,,,
+OOOOOOOOOOOOOx;'.'.''.........'......:llodddo:,'','''.........''...''''',,,,
+OOOOOOOOOOOOo,.....'.................':lodoc,.....'..........'''''.....'',,,
+kOOOOOOOOOkl,'...................'..'.';ol;,'. .............''''''''...'''',
+kkkkkkkkkkc'''..........................''.............'....'''''.''.....'',
+kkkkkkkkkc'...................'.......................''....'''''.''.'''...,
+      ██╗    ██╗██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗ █████╗ 
+      ██║    ██║╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+      ██║ █╗ ██║ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║███████║
+      ██║███╗██║  ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║██╔══██║
+      ╚███╔███╔╝   ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║██║  ██║
+       ╚══╝╚══╝    ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝
+"""
+
+
+defeat = """\033[31m
+KKKKKKKKKKKK00000000000000000000OOOOOkxddoodxkkkkkxxxxxxxxxxxxxxxxxxxxx
+KKKKKKKKKKKK0000000000000000000OOxdollccc::ccoddddddxxxxxxxxxxxxxxxxxxx
+KKKKKKKKKKKK0000000000OOxdooooc;,,'''''''''',;:::;;;:ldxxxxxxxxxxxxxxxx
+KKKKKKKKKKKK0000000ko:;,'''........'''''''''.......',,;ldxxxxxxxxxxxxxx
+KKKKKKKKKKK000000Oo,'''..'''''''...,,'....'''''......',',cxxxxxxxxxxxxx
+KKKKKKKKKKK00000kxc,'..........''''''''............... ...:ddxxxxxxxxxx
+KKKKKKKKKKK0000Oxoc,'''.'....''',;:;;,'......  .......  ....,:dxxxxxxxx
+KKKKKKKKKKK00000kdc;,;,...''',,:codooc:,'......           ...'cdxxxxxxx
+KKKK000KKKK00000kd:,'''.''',;cloxkOOkxdl:,'......          ...cddxddddd
+KKKK000000000000Odl:;;,,,;:coxkO0000OOkxo;'.......          ...:odddddd
+KKK00000000000000kdlc:::loddkOOOO000OOOxoc;'.......         .'..;dddddd
+KKK0000000000000OxooddodxkkkOOOOOOOOOOOxoc;'.......          .;''oddddd
+0000000000000000OxdoxxxkkOOOOOOOOOOOOOkxoc;'......    .'..    .;.,oolc:
+0000000000000000OkddkO000000xkkkkkOOOOkxo:;,'...     ......    .,......
+00000000000000000O0K00000OO0kxkkOOOOOOOkxdc;'...     ......     .'.....
+000000000000000KKKKKKXKkk0K0kodkOOOOkkkkkxdo:....    ':::.       '.....
+0000000Okk00000000KKK0kOKKOxlcoxkkkkxxxxxddl;....    .'',.       ......
+000000Okxlx00000000OkkKXKOl;::ldddddddooc;'';ll:'                 .    
+000000OkdcxO000000OxO000kod0x'';cllol;,;codddxddc.                     
+00000OkxdO000O00OkxOOOkxd0K0d,'';:;';oxkkkxxxxddoc'                    
+O000OkkkO000000OxdOOOkdoO0kd,';:cclockkkkkxxxxddooc'.           .      
+OOOOOkkxkkO0000OOO0Oxox0Okdlc;;:cloodxkkxxxxxddooooc;,'....    ..'.....
+OOOOOkkxxxxOOOOOOOOkokOkxolllcc::cloddddxxxxddddddolc::;::c:'  ........
+OOOkkxddddddxkkOOOOkOOkxdllllll:::loddddddddddddddoolcccccc:...........
+kkkkkddddddddxxkkOOkkkxdccl:;;;:c::coodddxddddddddollc:lllc'...........
+kkkkkxddxddddxxxxkkkkxddd;,'',;ccc::clooooddddddool::;clll;............
+kkkkxxddxxxxxxxxxxxxxdoc,...'',;;:::cclooooooooolc:,,:cllc'............
+xxxxxxxddxxxxxxxxxxddo:........'',;:cccccccccccc:;'';clcc;.............
+xxxxxdxxdxxxxxxxxxddol'..........',::cccc:;;,''''.';::cc:'.............
+xxxdddxxxxxxxxxxdddoc'............',,,;;'........',;::c:'....... ......
+xxxdddxxxxxxxdddool:'..  ........ ......   ...',,;;:::;'...............
+xxoooddxxxxxdddolc;....  ......            .',;;;::::;'................
+dolooodxxxxdddol:'............        ........',;;;;,..................
+dlloooodxxdddolc'................        .......,;,,...................
+██████╗ ██████╗ ███████╗███████╗ ██████╗ ██████╗  █████╗ ███╗   ██╗ █████╗ 
+██╔══██╗██╔══██╗╚══███╔╝██╔════╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+██████╔╝██████╔╝  ███╔╝ █████╗  ██║  ███╗██████╔╝███████║██╔██╗ ██║███████║
+██╔═══╝ ██╔══██╗ ███╔╝  ██╔══╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║██╔══██║
+██║     ██║  ██║███████╗███████╗╚██████╔╝██║  ██║██║  ██║██║ ╚████║██║  ██║
+╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝                                                                       
+"""
+
+baner = """
+ddddddddddddddddddddddddddddddddddddddollooooooodxxxxkkkkkkkkkkkkkkkkkkkkkxxxxdooooooollodddddddddddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddddooccccc,',;,,,,:ccldxxkkkkkkkkkkkkkkkkkkkkkxxdlcc:,,,,;,',cccccooddddddddddddddddddddddddddddddd
+ddddddddddddddddddddddddddoc;;;,'''..''''''''',,,:lxxkkkkkkkkkkkkkkkkkkkkkxxl:,,,'''''''''..''',;;;codddddddddddddddddddddddddd
+dddddddddddddddddddddddddl;,''''''''''''''''''',,,;lxxkkkkkkkkkkkkkkkkkkkxxl;,,,''''''''''''''''''',;lddddddddddddddddddddddddd
+dddddddddddddddddddddddol;'''..'''',;:::::;;,'',,;:cdxkkkkkkkkkkkkkkkkkkkxdl:;,,'',;;:c:::;,''''..''';loddddddddddddddddddddddd
+ooddddddooooooooooddddol,......''',;codddoolc:;;;::coxkkkkkkkkkkkkkkkkkkkxoc::;;;:cloodddoc;,'''......,loddddooooooooodddddddoo
+oooooooooooooodooddodoo:'.......'',coxxkkkkkkxdolollodxkkkkkkkkkkkkkkkkkxdollolodxkkkkkkxxoc,''.......':ooooddooooooooooooooooo
+oooooooooooooooooooooooc.........';coxxkkkkkkkkxxxdooxkkkkkkkkkkkkkkkkkkkxooddxxkkkkkkkkxxoc;'.........cooooooooooooooooooooooo
+ooooooooooooooooooooooo;.........',:oxxxxkkkkkxxxxxdoxkkkkkkkkkkkkkkkkkkkxodxxxxxkkkkkxkxxo:,'.........;ooooooooooooooooooooooo
+ooooooooooooooooooooool;.........':odxxxxkkkkkkxxxxdloxkkkkkkkkkkkkkkkkkkoldxxxxkkkkkkxxxxdo:'.........;loooooooooooooooooooooo
+ooooooooooooooooooooool:'........';codddxxdddooooooodxkkkkkkkkkkkkkkkkkkkxdooooooodddxxdddoc;'........':loooooooooooooooooooooo
+lllllllllooooooooooolllc..,'''',;clcllc::c;,,;;ll;,:lcokkkkkkkkkkkkkkkkkocl:,;cl;;,,;c::cllclc;,'''',..clllooooooooooolllllllll
+lllllllllllllllcclllllll,.':;;:clodddxxooolll:cddccoooxkkkkkkkkkkkkkkkkkxoooccddc:llloooxxdddolc:;;:'.,lllllllcclllllllllllllll
+cc:;...................'...,lllloodddxddddooooddxoldxkkkkkkkkkkkkkkkkkkkkkxdloxddooooddddxdddoollll,...'...................;:cc
+'...........................,cllooodddddddolllodddldxkkkkkkkkkkkkkkkkkkkkkxdldddolllodddddddooollc,...........................'
+............................:lccloooooollcllc::clllxxxkkkkkkkkkkkkkkkkkkkxxxlllc::cllclloooooolccl:............................
+......  .  .       .......'cll:::clloooolcccc:;:clxxxxxkkkkkkkkkkkkkkkkkxxxxxlc:;:ccccloooollc:::llc'......        .  .  ......
+.....     .    ...........'lolc;,,:cccloooolc::coxxxxxxxkkkkkkkkkkkkkkkxxxxxxxoc::cloooolccc:,,;clol'...........    .     .....
+...         .',''''''......,lllc:,',;;:cloooooodxkkkkxxxxxxxxxxxxxxxxxxxxxkkkkxdoooooolc:;;,',:clll,......'''''','.         ...
+...       .','''''.'........,lllcc:,''''coxxkkkkkkkkkkkxxxxxxxxxxxxxxxxxkkkkkkkkkkkxxoc'''',:cclll,........'.''''','.       ...
+..       .''''''.'...........,lllcc:;,'';ldxxxxxxxxkkkkxdxxxxxxxxxxxxxdxkkkkxxxxxxxxdl;'',;:cclll,...........'.''''''.       ..
+..   .. .','''.'..............':llcc:;,,,codddddxxxkkkkdddddddddddddddddkkkkxxxdddddoc;,,;:ccll:'..............'.''','. ..   ..
+...... .',''''..................,cc::;'..,,';ccllloxxkkdddddddddddddddddxkxxolllcc;',,..';::cc,..................'''','. ......
+...    .'''''''...................'','.....';:ccclodxxxdddddddddddddddddxxxdolccc:;'.....',''...................'''''''.    ...
+...    .'''''......................................':llloooodddddddoooolll:'......................................'''''.    ...
+...   .''''''........................................;clllllooooooolllllc;........................................''''''.   ...
+.    ..''''''.........................................';::clooolloolc::;'.........................................''''''..    .
+     .''''..............................................',;:clccclc:;,'..............................................''''.     
+    .'''''.....................................    ......,;:::::::::;,......    .....................................'''''.    
+   .'''''....................................      ......';;:::::::;;'.....     . ....................................'''''.   
+              ____  ___     _     ____   _   _  _____        ____   _      ___  __/_/ _   _  ___     _     _  __ ___ 
+             / ___||_ _|   / \   / ___| | \ | || ____|      | __ ) | |    |_ _||__  /| \ | ||_ _|   / \   | |/ /|_ _|
+            | |     | |   / _ \  \___ \ |  \| ||  _|        |  _ \ | |     | |   / / |  \| | | |   / _ \  | ' /  | | 
+            | |___  | |  / ___ \  ___) || |\  || |___       | |_) || |___  | |  / /_ | |\  | | |  / ___ \ | . \  | | 
+             \____||___|/_/   \_\|____/ |_| \_||_____|      |____/ |_____||___|/____||_| \_||___|/_/   \_\|_|\_\|___|
+                                                                                                           
+"""
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from kns.twins import Twins
-from kns.utils import has_twins, test_candidates_for_position, add_number_to_permutation
+from kns.utils import has_twins, test_candidates_for_position, add_number_to_permutation, victory, defeat, baner
 import random
 
 
 def main():
+    print(baner)
     n = input("Podaj dlugosc permutacji: ")
     n = int(n)
     k = input("Podaj minimalną długość ciasnego bliźniaka (domyślnie 2): ")
@@ -17,11 +18,18 @@ def main():
     out = loop(n, k)
     if out:
         print("Wygrał gracz")
+        print(defeat)
     else:
         print("Wygrał człowiek")
+        print(victory)
 
 
 def loop(n, k):
+
+    CP1 = '\033[36m'
+    CP2 = '\033[35m'
+    CEND = '\33[0m'
+
     current_permutation = []
     available_numbers = [i for i in range(1, n+1)]
     while len(available_numbers):
@@ -35,10 +43,23 @@ def loop(n, k):
         else:
             current_permutation = add_number_to_permutation(current_permutation, idx, available_numbers[0])
             available_numbers.remove(available_numbers[0])
-            print("Permutacja: %s" % str(current_permutation))
+
             twins = Twins()
             has_twins(current_permutation, k, twins)
-            print("Ciasne bliźniaki: %s, %s" % (str(twins.p1), str(twins.p2)))
+
+            str_perm = []
+            for x in current_permutation:
+                if x in twins.p1:
+                    str_perm.append(CP1 + str(x) + CEND)
+                elif x in twins.p2:
+                    str_perm.append(CP2 + str(x) + CEND)
+                else:
+                    str_perm.append(str(x))
+
+            str_perm = ', '.join(str_perm)
+                
+            print("Permutacja: [%s]" % str_perm)#str(current_permutation))
+            print("Ciasne bliźniaki: %s, %s" % (CP1 + str(twins.p1) + CEND, CP2 + str(twins.p2) + CEND))
             return True
 
     print("Permutacja: %s" % str(current_permutation))
@@ -48,18 +69,24 @@ def loop(n, k):
 def get_position(max_pos):
     while True:
         k = input("Podaj pozycję: ")
-        k = int(k)
-        if k < 0 or k > max_pos:
+        if k == "" or int(k) < 0 or int(k) > max_pos:
             print("Niepoprawna pozycja")
         else:
-            return k
+            return int(k)
 
 
 def print_current_permutation(permutation):
+
+    CPOZ = '\033[128m'
+    CEND = '\033[0m'
+    CVAL = '\33[93m'
+
     out = ""
     for i, elem in enumerate(permutation):
-        out += " [" + str(i) + "] " + str(elem)
-    out += " [" + str(len(permutation)) + "]"
+        out += CPOZ + " [" + str(i) + "] " + CEND + CVAL + str(elem) + CEND
+    out += CPOZ + " [" + str(len(permutation)) + "]" + CEND
+    permutation_str = ', '.join(CVAL + str(x) + CEND for x in permutation)
+    out += '\nObecna permutacja: ' + permutation_str
     print(out)
 
 
